397ce18550318cb3dd2a75f688d5c0ce6672a277
Alexander <alexkorpol1964@gmail.com>
2023-05-22 01:12:33 +0300

    Finished 4-th Task

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,32 +1,36 @@
-// import './App.css';
 import user from '../../data/user.json';
 import data from '../../data/data.json';
 import friends from '../../data/friends.json';
+import transactions  from '../../data/transactions.json';
 import { Profile } from '../Profile/Profile';
 import { Statistics } from '../Statistics/Statistics';
 import { TaskNumber } from 'components/Profile/TaskNumber.styled';
 import { FriendList } from 'components/FriendList/FriendList';
-// import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+import {TransactionHistory} from 'components/TransactionHistory/TransactionHistory'
 
 export const App = () => {
   return (
     <>
-    <TaskNumber> Task 1  </TaskNumber>
-    <Profile
+      <TaskNumber> Task 1  </TaskNumber>
+      <Profile
       username={user.username}
       tag={user.tag}
       location={user.location}
       avatar={user.avatar}
       stats={user.stats}
       />
-    <TaskNumber> Task 2  </TaskNumber>
-    <Statistics
+      <TaskNumber> Task 2  </TaskNumber>
+      <Statistics
       title="Upload stats"
       stats={data}
       />
-    <TaskNumber> Task 3  </TaskNumber>
+      <TaskNumber> Task 3  </TaskNumber>
       <FriendList
         friends={friends}
+      />
+      <TaskNumber> Task 4  </TaskNumber>
+      <TransactionHistory
+        items={transactions}
       />
     </>
     )
---
+++ src/components/TransactionHistory/TransactionHistory.styled.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+
+export const TransactionHistoryTable = styled.table`
+  background-color: #fff;
+  width: 600px;
+  margin: 60px auto;
+  padding: 30px;
+  border-radius: 10px;
+  box-shadow: 1rem 1rem 2rem rgb(65, 64, 64), -1rem -1rem 2rem rgb(82, 81, 81);
+`;
+export const TableHeader = styled.th`
+  height: 50px;
+  text-align: center;
+  border: 1px solid #555;
+  background-color: #499fc4;
+  color: #fff;
+  font-size: 14px;
+  text-transform: uppercase;
+`;
+export const TableData = styled.td`
+  height: 50px;
+  text-align: center;
+  border: 1px solid #555;
+`;
+export const TableRow = styled.tr`
+  :nth-of-type(even) {
+    background-color: #fff;
+    text-transform: uppercase;
+  }
+  :hover {
+    cursor: pointer;
+    background-color: #9998a3;
+  }
+`;
